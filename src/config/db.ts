c5816a79dc33f3bcc3767506f79a8b1f9ac483bb
Haroldo <haroldobeyerbacellar@gmail.com>
2021-05-02 15:41:53 -0300

    Adding database connection

---
+++ src/config/db.ts
@@ -0,0 +1,14 @@
+import { createConnection } from 'typeorm';
+
+export const connectServerWithDB = async () => {
+    const connection = await createConnection();
+    console.log(`Connected with DB ${connection.options.database}`);
+
+    process.on("SIGINT", () => {
+        connection.close().then(() => {
+            console.log(`Connection closed`);
+        }).catch((err) => {
+            console.log(`Error while trying to close connection: ${err}`);
+        });
+    });
+};
